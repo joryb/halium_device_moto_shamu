d23b5358aefd49c70763f3e251be479551b551e2
joryb <jb@droidbox>
2018-05-05 14:44:26 -0400

    Cleanup

--- aosp_shamu.mk
+++ aosp_shamu.mk
@@ -28,13 +28,9 @@ PRODUCT_DEVICE := shamu
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := AOSP on Shamu
 PRODUCT_MANUFACTURER := motorola
-#PRODUCT_RESTRICT_VENDOR_FILES := true
 
 $(call inherit-product, device/moto/shamu/device.mk)
 $(call inherit-product-if-exists, vendor/motorola/shamu/shamu-vendor.mk)
 
 PRODUCT_NAME := aosp_shamu
-
-PRODUCT_PACKAGES += \
-    Launcher3
 
